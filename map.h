4d79479716f13fa902246e2871c1c56835425e3f
Pocon041 <1561061781@qq.com>
2024-08-20 19:55:26 +0800

    Update map.h

--- map.h
+++ map.h
@@ -7,12 +7,15 @@ public:                      //为了方便我都写public了
 	int entY;   //起点位置
 	int exitX;  //终点位置
 	int exitY;  //终点位置
+	int currX;		//现在的位置
+	int currY;		//现在的位置
 	int sizeX;  //地图大小
 	int sizeY;  //地图大小
+	//bool end;   //表示地图是否进度百分百
 	std::string cood[5][5];   //坐标数组，用来记录每个地点的坐标。
 	
 	Map(int id_map,int sizeX,int sizeY,int entX,int entY,int exitX, int exitY);
 	void printMap(int id_map);
 	void setMap(int x, int y);					//x,y为当前位置,用于改变当前所在位置
-	void changePos(char dir, int& x, int& y);	//dir是direction的缩写，表示方向。用于转化wasd指令
+	void changePos(char dir);	//dir是direction的缩写，表示方向。用于转化wasd指令
 };
