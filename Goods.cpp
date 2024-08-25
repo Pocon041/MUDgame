85fd342cee91a20673915c315a25c26d7ccde221
Pocon041 <1561061781@qq.com>
2024-08-25 11:57:20 +0800

    Create Goods.cpp

---
+++ Goods.cpp
@@ -0,0 +1,367 @@
+#include <iostream>
+#include <string>
+#include "Goods.h"
+
+using namespace std;
+
+
+Weapon::Weapon(int addAttack, int addDefend) {
+	switch (id) {
+
+	case 0: {
+		id = 0;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 1: {
+		id = 1;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 2: {
+		id = 2;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 3: {
+		id = 3;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 4: {
+		id = 4;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 5: {
+		id = 5;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 6: {
+		id = 6;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 7: {
+		id = 7;
+		name = "柴刀";
+		desc = "山林樵夫用来砍柴的刀，做工粗糙，勉强可以用作武器";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	default: break;
+
+	}
+}
+
+Potion::Potion(int addHP, int addMP) {
+	switch (id) {
+
+	case 8: {
+		id = 8;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 9: {
+		id = 9;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 10: {
+		id = 10;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 11: {
+		id = 11;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 12: {
+		id = 12;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+
+	}
+	case 13: {
+		id = 13;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 14: {
+		id = 14;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 15: {
+		id = 15;
+		name = "金疮药";
+		desc = "常见的疗伤药，补充少量气血";
+		addHP = 0;
+		addMP = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	default: break;
+
+	}
+}
+
+Armor::Armor(int addAttack, int addDefend) {
+	switch (id) {
+
+	case 16: {
+		id = 16;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 17: {
+		id = 17;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 18: {
+		id = 18;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 19: {
+		id = 19;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 20: {
+		id = 20;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 21: {
+		id = 21;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 22: {
+		id = 22;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	case 23: {
+		id = 23;
+		name = "青布衣";
+		desc = "普通人家所穿的服饰，面料与裁剪都很普通";
+		addAttack = 0;
+		addDefend = 0;
+		priceSell = 0;
+		priceBuy = 0;
+		break;
+	}
+	default: break;
+
+	}
+}
+
+Item::Item(std::string name, int priceSell, int priceBuy) : name(name), priceSell(priceSell), priceBuy(priceBuy) {}
+
+Item::Item() {}
+
+void Item::showItem(){
+	cout << name << endl;
+	cout << desc << endl;
+	if (id >= 0 && id <= 7) { //武器
+		cout << "攻击力：" << getaddAttack() << endl;
+		cout << "防御力：" << getaddDefend() << endl;
+	}
+	else if (id <= 15) { // 药品
+		cout << "增加气血：" << getaddHP() << endl;
+		cout << "增加内力：" << getaddMP() << endl;
+	}
+	else { // 防具
+		cout << "攻击力：" << getaddAttack() << endl;
+		cout << "防御力：" << getaddDefend() << endl;
+	}
+}
+
+Goods::Goods(std::string name, std::string desc, int priceSell, int priceBuy, int id): name(name), desc(desc), priceSell(priceSell), priceBuy(priceBuy), id(id) {}
+
+Goods::Goods() {}
+
+string Goods::getName() {
+	return name;
+}
+
+string Goods::getDesc() {
+	return desc;
+}
+
+int Goods::getpriceSell() {
+	return priceSell;
+}
+
+int Goods::getpriceBuy() {
+	return priceBuy;
+}
+
+int Goods::getId() {
+	return id;
+}
+
+Goods::~Goods() {}
+
+Weapon::Weapon(int addAttack, int addDefend) : addAttack(addAttack), addDefend(addDefend) {}
+
+int Weapon::getaddAttack() {
+	return addAttack;
+}
+
+int Weapon::getaddDefend() {
+	return addDefend;
+}
+
+
+Weapon::~Weapon(){}
+
+Potion::Potion(int addHP, int addMP) : addHP(addHP), addMP(addMP) {}
+
+int Potion::getaddHP() {
+	return addHP;
+}
+
+int Potion::getaddMP() {
+	return addMP;
+}
+
+
+Potion::~Potion() {}
+
+Armor::Armor(int addAttack, int addDefend) : addAttack(addAttack), addDefend(addDefend) {}
+
+int Armor::getaddAttack() {
+	return addAttack;
+}
+
+int Armor::getaddDefend() {
+	return addDefend;
+}
+
+Armor::~Armor() {}
+
+
